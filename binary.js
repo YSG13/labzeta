71f7b18b29d6180937f9eb92c660246370b03e3d
YSG13 <yousify.talabani2012@gmail.com>
2025-08-05 13:50:07 +0300

    Update binary.js

--- binary.js
+++ binary.js
@@ -1,30 +1,33 @@
-const canvas = document.createElement("canvas");
+const canvas = document.getElementById("binary");
 const ctx = canvas.getContext("2d");
-document.body.appendChild(canvas);
-canvas.style.position = "fixed";
-canvas.style.top = 0;
-canvas.style.left = 0;
-canvas.style.zIndex = "-1";
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const cols = canvas.width / 20;
-const ypos = Array.from({ length: cols }).fill(0);
+const binary = "01";
+const fontSize = 16;
+const columns = canvas.width / fontSize;
 
-function binaryRain() {
-  ctx.fillStyle = "rgba(11,19,43,0.15)";
+const drops = [];
+for (let x = 0; x < columns; x++) drops[x] = 1;
+
+function draw() {
+  // dark navy blue w/ fade
+  ctx.fillStyle = "rgba(14, 28, 60, 0.1)";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  ctx.fillStyle = "#ccc";
-  ctx.font = "18px monospace";
+  ctx.fillStyle = "#ffffff";
+  ctx.font = fontSize + "px monospace";
 
-  ypos.forEach((y, index) => {
-    const text = Math.random() > 0.5 ? "1" : "0";
-    const x = index * 20;
-    ctx.fillText(text, x, y);
-    ypos[index] = y > canvas.height + Math.random() * 100 ? 0 : y + 20;
-  });
+  for (let i = 0; i < drops.length; i++) {
+    const text = binary[Math.floor(Math.random() * binary.length)];
+    ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+
+    if (drops[i] * fontSize > canvas.height && Math.random() > 0.975)
+      drops[i] = 0;
+
+    drops[i]++;
+  }
 }
 
-setInterval(binaryRain, 60);
+setInterval(draw, 33);
